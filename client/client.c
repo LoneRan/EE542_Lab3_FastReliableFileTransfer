aec769cddecec85448e63aa8db3bc8c2000247df
Yibo Wang <yibow@usc.edu>
2020-09-08 22:48:40 -0700

    add threads

--- client/client.c
+++ client/client.c
@@ -5,7 +5,60 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mutex.h>
 #include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <signal.h>
 
-//todo main and inplement multithreads
+//time of start of transmit
+struct timeval startAll;
+
+//send data to receiver
+pthread_t sendData;
+//send ACK back for every socket received
+pthread_t sendACK;
+//handle ACK: freeze timer
+pthread_t handleACK;
+//when a socket time out, retransmit
+pthread_t retransmit;
+
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+
+
+void *SendData(){
+
+}
+
+void *SendACK(){
+
+}
+
+void *HandleACK(){
+
+}
+
+void *Retransmit(){
+
+}
+
+
+int main(int argc, char *argv[]) {
+//todo socket
+    pthread_create(&sendData, NULL, SendData, NULL);
+    pthread_create(&sendACK;, NULL, SendACK, NULL);
+    pthread_create(&handleACK, NULL, HandleACK, NULL);
+    pthread_create(&retransmit, NULL, Retransmit, NULL);
+
+    gettimeofday(&startAll, NULL);
+
+    pthread_join(sendData, NULL);
+    pthread_join(sendACK, NULL);
+    pthread_join(handleACK, NULL);
+    pthread_join(retransmit, NULL);
+}
--- server/server.c
+++ server/server.c
@@ -5,7 +5,51 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mutex.h>
 #include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <signal.h>
 
-//todo main and inplement multithreads
+
+//receive data from sender
+pthread_t recData;
+//send ACK back for every socket received
+pthread_t sendACK;
+//rearrange the packet received, check if receive all the data
+pthread_t rearrange;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+
+
+void *RecData(){
+
+}
+
+void *SendACK(){
+
+}
+
+
+void *Rearrange(){
+
+}
+
+
+int main(int argc, char *argv[]) {
+//todo socket
+    pthread_create(&revData, NULL, RevData, NULL);
+    pthread_create(&sendACK;, NULL, SendACK, NULL);
+    pthread_create(&rearrange, NULL, Rearrange, NULL);
+
+    gettimeofday(&startAll, NULL);
+
+    pthread_join(revData, NULL);
+    pthread_join(sendACK, NULL);
+    pthread_join(rearrange, NULL);
+
+}
